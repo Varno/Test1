dd6da9decf4aa354c58f4631ddd69c71f00c2be4
Alexey Okishev <Alexey_Okishev@epam.com>
2014-11-28 11:57:36 +0400

    master

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApplication1
         // master changes
         static void Main(string[] args)
         {
+            //1
+            //2
+            //3
+            //4
         }
     }
 }
