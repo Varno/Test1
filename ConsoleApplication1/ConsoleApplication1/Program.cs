52753c8958a18b498df0c690d818378c3fc6170b
Alexey Okishev <alexey.okishev@live.ru>
2015-02-01 17:02:44 +0400

    reset soft 2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApplication1
         // master changes
         static void Main(string[] args)
         {
+            //1
+            //2
+            //3
+            //4
+            // dfsdfsdfsdf
         }
     }
 }
